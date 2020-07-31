b8b4f573837513a167cc8890ea766d03d74e535e
Sunbridger <739272884@qq.com>
2020-07-31 23:08:05 +0800

    feat: 利用webpack api自动生成路由

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
         alias: {
             src: resolve('./src'),
             api: resolve('./src/api'),
+            views: resolve('./src/views'),
             core: resolve('./src/core'),
             styles: resolve('./src/styles'),
             components: resolve('./src/components'),
