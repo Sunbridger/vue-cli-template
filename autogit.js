b963aa07d639394d9885d6704559e0c63fff6227
Sunbridger <739272884@qq.com>
2020-08-17 09:45:29 +0800

    feat: 定时任务

--- autogit.js
+++ autogit.js
@@ -1,35 +1,44 @@
 const { exec } = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const Schedule = require('node-schedule');
 const newList = [];
 const newversion = Math.random().toString(36).slice(-8);
+const rule = new Schedule.RecurrenceRule();
+rule.hour = 0;
+rule.minute = 0;
+rule.second = 0;
 
-const handGIT = () => {
-    exec('git add .', (err) => {
-        console.log(err);
-        if (!err) {
-            exec(`git commit -m "update: version:${newversion}"`, (err) => {
-                exec('git push', (err) => {
-                    console.log(err, '---err');
+// 启动任务
+let job = schedule.scheduleJob(rule, () => {
+    const handGIT = () => {
+        exec('git add .', (err) => {
+            if (!err) {
+                exec(`git commit -m "update: version:${newversion}"`, (err) => {
+                    exec('git push', (error) => {
+                        if (error) {
+                            console.log(error, '---git push err');
+                        }
+                    })
                 })
-            })
-        }
-    });
-}
-
-fs.readFile(path.join(__dirname, './package.json'), 'utf8', (err, data) => {
-    if (err) {
-        console.log(err, '读取package失败');
-    } else {
-        let obj = JSON.parse(data);
-        obj.version = newversion;
-        obj = JSON.stringify(obj);
-        fs.writeFile(path.resolve(__dirname, './package.json'), obj, 'utf8', (error, data) => {
-            if (error) {
-                console.log(err, '修改package失败');
-            } else {
-                handGIT();
             }
         });
     }
-})
+
+    fs.readFile(path.join(__dirname, './package.json'), 'utf8', (err, data) => {
+        if (err) {
+            console.log(err, '读取package失败');
+        } else {
+            let obj = JSON.parse(data);
+            obj.version = newversion;
+            obj = JSON.stringify(obj);
+            fs.writeFile(path.resolve(__dirname, './package.json'), obj, 'utf8', (error, data) => {
+                if (error) {
+                    console.log(err, '修改package失败');
+                } else {
+                    handGIT();
+                }
+            });
+        }
+    })
+});
